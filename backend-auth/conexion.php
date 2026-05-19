bedf6a07ba26b1d13e93faf080388982a9a1c94a
tristanlibreros-blip <tristanlibrerosgmail.com>
2026-05-19 16:49:12 -0500

    cambie cosas

--- backend-auth/conexion.php
+++ backend-auth/conexion.php
@@ -1,8 +1,8 @@
 <?php 
-$host = 'localhost';
-$user = 'root';
-$password = '';
-$database = 'chambatec';
+$host = 'sql105.infinityfree.com';
+$user = 'if0_41968401';
+$password = 'CpXsBB4n0Pc';
+$database = 'if0_41968401_chambatec';
 $conn = new mysqli($host, $user, $password, $database);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
